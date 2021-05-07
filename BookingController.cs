629db79bd4af979257f5e835ba39dc542e63a1cf
anmolabhishek <44404264+anmolabhishek@users.noreply.github.com>
2021-05-07 23:01:28 +0530

    Add files via upload

---
+++ BookingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RailwayReservationSystem.Models;
+
+namespace RailwayReservationSystem.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(Search model)
+        {
+            using (var context = new RailwayReservationSystemEntities())
+            {
+                /* var validation = from t in context.Train_Details
+                                  join s in context.Station on t.Train_No equals s.Train_No
+                                  select new { t.Source, s.Stn_Name };*/
+                if (context.Train_Details.Any(x => x.Source == model.Source && x.Destination == model.Destination && x.Start_Date == model.Doj) || context.Station.Any(y => y.Stn_Name == model.Source && y.Stn_Name==model.Destination))
+                {
+
+                    return RedirectToAction("PassengerDetails", "Book Now");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Enter Correct Details");
+                        return View();
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RailwayReservationSystem.Models
+{
+    public class Search
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage ="Enter Source")]
+        public string Source { get; set; }
+        [Required(ErrorMessage ="Enter Destination")]
+        public string Destination { get; set; }
+        [Required(ErrorMessage ="Enter Date Of Journey")]
+        [Display(Name="Date Of Journey")]
+        [DataType(DataType.Date)]
+        public  System.DateTime Doj { get; set; }
+    }
+}
